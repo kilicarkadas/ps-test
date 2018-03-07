10e7053485fd4f5c9535ff1b58705553575d1543
kilicarkadas <kilicarkadas@gmail.com>
2018-03-07 02:12:19 +0000

    Added ordering-api project and docker support.

---
+++ Ordering-Api/Models/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderingApi.Models
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ Ordering-Api/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace OrderingApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public Int64 CompanyId { get; set; }
+
+        public string TerminalName { get; set; }
+
+        public int TerminalNumber { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
